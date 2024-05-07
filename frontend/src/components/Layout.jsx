bf354b369751e1eb641a7c34568f455c56601937
AndreMooore <143398486+AndreMooore@users.noreply.github.com>
2024-05-07 14:31:38 +0200

    Pokeball image i logo
    Henta ned source til pokeball image.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,12 @@
-import Teams from "./Teams";
-import PokeCard from "./PokeCard";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Layout({children}) {
+
     return (
         <>
         <header>
-            <Link to={"/"}><img id="pokeball" src="#" alt="logo" />UIN Pokedex</Link>
+            <Link to={"/"}><img id="pokeball" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="logo" />UIN Pokedex</Link>
             <Link to={"/teams"}>Teams</Link>
             <input type="search" />
         </header>
