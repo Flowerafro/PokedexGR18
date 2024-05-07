16c5b3bb7a65df6bc26102ab17442b338bbfa7b5
MiriamKy <miri.kristiansen@gmail.com>
2024-05-07 23:42:58 +0200

    Start - utskrift av type-komponentet

--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -4,13 +4,13 @@ import { useParams } from "react-router-dom"
 
 export default function Type() {
 
+    const {slug} = useParams
+    
     // Gjøre en ny fetch i dette komponentet som bare henter ut de pokemon som stemmer med slugen som representerer typen som er klikket
+    // Prøver meg her på å hente verdien i slugen, men har ikke helt funnet ut av det ennå...
     return (
         <>
-        <h2>Type</h2>
-        {/* {pokemon?.filter((card, index, slug) => {
-            return <PokeCard  slug={slug} key={index} card={card}/>;
-            })} */}
+        <h2>{slug}</h2>
         </>
     )
 }
--- frontend/src/components/TypeCard.jsx
+++ frontend/src/components/TypeCard.jsx
@@ -1,14 +1,7 @@
-// Skal dette komponentet representere utskriften av type-knappene? Altså inneholde:
-
-//1: Mapping av typer som ligger i API-et <3
-//2: Utskrift av ikoner basert på slugen eller type.name
-//3: Mapping av farger som ligger i _color.scss
-//4: Sammenfletting av disse 3 mappene slik at vi får en utskrift av alle typekortene
-
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function TypeCard({}) {
+export default function TypeCard() {
 
     const [type, setType] = useState([])
 
