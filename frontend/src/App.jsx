a812f97df2deaf3cfe215ed27aaeeccfedbbb3c7
MiriamKy <miri.kristiansen@gmail.com>
2024-05-10 15:46:07 +0200

    Flyttet litt på fuknsjonalitet
    - I sammenheng med fetch og utskrift home-komponentet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,39 +12,15 @@ import SearchResults from './components/SearchResults'
 import { useEffect, useState } from 'react'
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
-  const [pokeData, setPokeData] = useState([])
-  const [limit, setLimit] = useState("?limit=9")
-  
-    const getPokemon = async () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon`)
-            .then(response => response.json())
-            .then(data => {
-                setPokemon(data.results);
-                data.results.forEach(poke => getPokemonData(poke.url));
-            })
-            .catch(error => console.error(error))
-    }
-
-    const getPokemonData = async (url) => {
-        fetch(url)
-          .then(response => response.json())
-          .then(data => setPokeData(prevState => [...prevState, data])) //https://chat.openai.com/share/c9fa4901-bfc8-474c-b193-d48614373831 Jeg brukte chathpt her for å få til å lagre på usestaten når det allerede er data på den uten å overskrive :)
-          .catch(error => console.error(error))
-    }
-
-    useEffect(() => {
-    getPokemon()
-    }, [])
     
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home pokeData={pokeData} setLimit={setLimit} getPokemon={getPokemon}/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/teams" element={<Teams />} />
           <Route path="/type/:slug" element={<Type />} />
-          <Route path="/pokemon/:slug" element={<Pokemon pokeData={pokeData} />} />
+          <Route path="/pokemon/:slug" element={<Pokemon />} />
           <Route path="/searchresults/:slug" element={<SearchResults />} />
         </Routes>
       </Layout>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,16 +2,34 @@ import PokeCard from "./PokeCard";
 import TypeCard from "./TypeCard";
 import { useEffect, useState } from "react";
 
-export default function Home({ setLimit, pokeData }) {
+export default function Home() {
+
+    const [pokemon, setPokemon] = useState([])
+    const [pokeData, setPokeData] = useState([])
     
-  useEffect(() => {
-    setLimit("?limit=9")
-  }, [])
+      const getPokemon = async () => {
+          fetch(`https://pokeapi.co/api/v2/pokemon?limit=9`)
+              .then(response => response.json())
+              .then(data => {
+                  setPokemon(data.results);
+                  data.results.forEach(poke => getPokemonData(poke.url));
+              })
+              .catch(error => console.error(error))
+      }
   
+      const getPokemonData = async (url) => {
+          fetch(url)
+            .then(response => response.json())
+            .then(data => setPokeData(prevState => [...prevState, data])) //https://chat.openai.com/share/c9fa4901-bfc8-474c-b193-d48614373831 Jeg brukte chathpt her for å få til å lagre på usestaten når det allerede er data på den uten å overskrive :)
+            .catch(error => console.error(error))
+      }
   
+      useEffect(() => {
+      getPokemon()
+      }, [])
+      
     return (
-        <>
-        
+        <> 
         {pokeData?.map((poke, index) => {
                     let paddedId;
                     if (poke.id < 10) {
@@ -30,7 +48,7 @@ export default function Home({ setLimit, pokeData }) {
                         />
                     );
                 })}
-    </>
+        </>
         
     )
 }
