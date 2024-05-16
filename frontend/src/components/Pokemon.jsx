d13007fb3833bf8997ed3aa0a8159b6d903fdf15
MiriamKy <miri.kristiansen@gmail.com>
2024-05-16 23:32:42 +0200

    Endret fetch til egen syntax
    - Bruker syntax fra home-komponentet i fetchen i pokemon-komponentet også, slik at det blir likere
    - Oppdatert utskrift i return til ny fetch

--- frontend/src/components/Pokemon.jsx
+++ frontend/src/components/Pokemon.jsx
@@ -4,79 +4,74 @@ import TypeCard from "./TypeCard"
 
 export default function Pokemon() {
 
-    const {slug} = useParams()
-    const [itAll, setItAll] = useState([])
-    
+  const {slug} = useParams()
+  const [details, setDetails] = useState()
+  const [abilities, setAbilities] = useState([])
 
-    const getItAll = () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${slug}/`)
-          .then(response => response.json())
-          .then(data => {
-            const abilities = data.abilities;
-            const promises = abilities.map(ability => 
-              fetch(ability.ability.url)
-                .then(response => response.json())
-                .then(abilityData => {
-                  const effectEntry = abilityData.effect_entries.find(entry => entry.language.name === 'en');
-                  const effect = effectEntry.effect;
-                  const shortEffect = effectEntry.short_effect;
-                  return { ...ability, description: { effect, shortEffect } };
-                })
-            );
-            Promise.all(promises)
-              .then(abilitiesWithDescriptions => {
-                setItAll({ ...data, abilities: abilitiesWithDescriptions });
-              });
-          })
-          .catch(error => console.error(error));
-      };  //Denne funksjonen var ikke lett å lage med tanke på ability descriptions. Så jeg måtte bruke copilot for å få til linje 19 til 28. Jeg legger ved bilde av chatten på dokumentet.
+  const getDetails = async () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${slug}/`)
+    .then(response => response.json())
+    .then(data => {
+        setDetails(data);
+        data.abilities.forEach(ability => getAbilities(ability.ability.url));
+        })
+    .catch(error => console.error(error))
+  }
 
-    useEffect(()=> {
-        getItAll()
-    }, [slug])
+  const getAbilities = async (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setAbilities(prevState => [...prevState, data]))
+    .catch(error => console.error(error))
+  }
 
-    console.log("itAll" + itAll)
+  useEffect(()=> {
+      getDetails()
+  }, [slug])
 
-    return (
-        <section id="pokemon">
-            {itAll.sprites && (
-                <>
-                <section>
-                <article className="pokemon-img">
-                <h1>{itAll.name.charAt(0).toUpperCase() + itAll.name.slice(1)}</h1>
-                <img src={itAll.sprites.other?.['official-artwork'].front_default} alt={itAll.name} />  
-                </article>
-                
-                <article className="pokemon-abilities">
-                <h1>Abilities</h1>
-                {itAll.abilities?.map((ability, index) => (
-                    <div key={index}>
-                    <h2>{ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)}</h2>
-                    <p>Effect: {ability.description.effect}</p>
-                    <p>Short Effect: {ability.description.shortEffect}</p>
-                    </div>
-                ))}
-                </article>
-                </section>
+  console.log("Details", details)
 
-                <section>
-                <article className="pokemon-type">
-                    <h1>Types</h1>
-                    {itAll.types?.map((type, index) => (<TypeCard key={index} name={type.type.name} />))}
-                </article>
+  return (
+    <section id="pokemon">
+      {details?.sprites && (
+        <>
+          <section>
+            <article className="pokemon-img">
+              <h1>{details.name.charAt(0).toUpperCase() + details.name.slice(1)}</h1>
+              <img src={details.sprites.other?.['official-artwork'].front_default} alt={details.name} />  
+            </article>
+            
+            <article className="pokemon-abilities">
+              <h1>Abilities</h1>
+              {abilities?.map((ability, index) => (
+                <div key={index}>
+                  <h2>{ability.name.charAt(0).toUpperCase() + ability.name.slice(1)}</h2>
+                  <p>Effect: {ability.effect_entries[0].effect}</p> {/* Filtrere etter engelsk */}
+                  <p>Short Effect: {ability.effect_entries[0].short_effect}</p> {/* Filtrere etter engelsk */}
+                </div>
+              ))}
+            </article>
+          </section>
 
-                <article className="pokemon-stats">
-                  <h1>Stats</h1>
-                  {itAll.stats?.map((stat, index) => (
-                      <ul key={index} className="stats">
-                          <li>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: </li>
-                          <progress value={stat.base_stat} max="100"></progress>
-                          <li>{stat.base_stat}</li>
-                      </ul>
-                  ))}
-              </article>
-                </section>
-                </>
-            )}
-        </section>
-    )}
+          <section>
+            <article className="pokemon-type">
+              <h1>Types</h1>
+              {details.types?.map((type, index) => (<TypeCard key={index} name={type.type.name} />))}
+            </article>
+
+            <article className="pokemon-stats">
+              <h1>Stats</h1>
+              {details.stats?.map((stat, index) => (
+                <ul key={index} className="stats">
+                  <li>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: </li>
+                  <progress value={stat.base_stat} max="100"></progress>
+                  <li>{stat.base_stat}</li>
+                </ul>
+              ))}
+            </article>
+          </section>
+        </>
+      )}
+    </section>
+  )
+}
